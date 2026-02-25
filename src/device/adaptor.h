726cd841b4769886d7b8fb0712c1c7e415dc1eed
Hongbo Li <lihb2113@outlook.com>
2026-02-25 11:41:25 +0800

    Init code.
    Signed-off-by: Hongbo Li <lihb2113@outlook.com>

---
+++ src/device/adaptor.h
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Created by Hongbo Li <lihb2113@outlook.com>
+ */
+#ifndef __ADAPTOR_H__
+#define __ADAPTOR_H__
+
+#include "device.h"
+#include <cstdint>
+
+class Adaptor
+{
+public:
+    virtual ~Adaptor() {};
+
+    virtual void*
+        AllocPinnedPtr(uint8_t nid, uint64_t size, unsigned int flags, void **daddr) = 0;
+
+    virtual void
+        FreePinnedPtr(void *addr, uint64_t size) = 0;
+
+    virtual void*
+        WrapperPtr(uint8_t nid, void *addr, uint64_t size) = 0;
+
+    virtual void
+        UnWrapperPtr(void *daddr) = 0;
+
+    virtual void*
+        OpenIPCKeys(uint8_t nid, uint16_t per_len, uint16_t num, void *data) = 0;
+
+    virtual void
+        CloseIPCKeys(void *daddr) = 0;
+
+    virtual int
+        TransferKVCache(struct transfer_params *params) = 0;
+
+    virtual void
+        Reset(uint8_t nid) = 0;
+};
+
+#endif
